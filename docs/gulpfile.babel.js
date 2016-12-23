8d6cdb115fa1731d14329bdd8f09cd53bbaf1c16
harry827 <langzhi@jiankongbao.com>
2016-12-23 12:25:54 +0800

    gulpfile to ES6

--- docs/gulpfile.babel.js
+++ docs/gulpfile.babel.js
@@ -1,21 +1,24 @@
-var gulp = require('gulp');
-var fileinclude = require('gulp-file-include');
-var clean = require('gulp-clean');
-var rename = require('gulp-rename');
-var minifyCss = require('gulp-minify-css');
+import gulp from 'gulp';
+import fileinclude from 'gulp-file-include';
+import clean from 'gulp-clean';
+import rename from 'gulp-rename';
+import minifyCss from 'gulp-minify-css';
+import pkg from './package.json';
 
-var pkg = require('./package.json');
-
+const paths = {
+    src: './_src',
+    dest: '.',
+};
 /*
  * html
  */
-gulp.task('clean_html', function () {
+gulp.task('clean_html', ()=> {
     return gulp.src('pages')
         .pipe(clean({force: true}));
 });
 
-gulp.task('html', ['clean_html'], function () {
-    gulp.src(['_src/pages/*.html'])
+gulp.task('html', ['clean_html'], ()=> {
+    gulp.src([`${paths.src}/pages/*.html`])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file',
@@ -27,42 +30,41 @@ gulp.task('html', ['clean_html'], function () {
                 page: null
             }
         }))
-        .pipe(gulp.dest('pages/'));
+        .pipe(gulp.dest(`${paths.dest}/pages/`));
 });
 
 /*
  * css
  */
-gulp.task('clean_css', function () {
-    return gulp.src('css')
+gulp.task('clean_css', ()=> {
+    return gulp.src(`${paths.dest}/css`)
         .pipe(clean({force: true}));
 });
 
-gulp.task('css', ['clean_css'], function () {
-    gulp.src('_src/css/docs.css')
+gulp.task('css', ['clean_css'], ()=> {
+    gulp.src(`${paths.src}/css/docs.css`)
         .pipe(rename({suffix: '.min'}))
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest("css/"));
+        .pipe(gulp.dest(`${paths.dest}/css/`));
 });
 
 /*
  * 文件自动监听
  */
-gulp.task('watch', ['default'], function () {
+gulp.task('watch', ['default'], ()=> {
     // 监听html模板
-    gulp.watch(['_src/pages/**/*.html'], ['html'])
+    gulp.watch([`${paths.src}/pages/**/*.html`], ['html'])
         .on('change', function (event) {
             console.log('html模板文件改变！');
         });
 
     // 监听css文件
-    gulp.watch(['_src/css/**/*.css'], ['css'])
-        .on('change', function (event) {
+    gulp.watch([`${paths.src}/css/**/*.css`], ['css'])
+        .on('change', (event)=> {
             console.log('css文件改变！');
         });
 });
 
-gulp.task('default', ['html', 'css'], function () {
+gulp.task('default', ['html', 'css'], ()=> {
     console.log('build done!');
-})
-;
+});
