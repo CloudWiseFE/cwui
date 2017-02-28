3e6a582a5701a19532919c0656def37a5fb9f8a3
caiting <jenny.pei@yunzhihui.com>
2017-02-28 18:18:13 +0800

    新加模块---

--- docs/_src/js/code.js
+++ docs/_src/js/code.js
@@ -3,7 +3,6 @@ seajs.use([], function () {
         var code_id = $('.code').eq(i).data('target');
         var html = $('#'+code_id).html();
         var readyTemplate =html;
-        console.log(readyTemplate);
         codebeautifier({
             element: $('[data-target='+code_id+']')[0],
             type: 'html',
--- src/js/components/collapse.js
+++ src/js/components/collapse.js
@@ -3,12 +3,12 @@ $(function(){
         var open = $(this).hasClass('open');
         if(open){
             $(this).removeClass('open');
-            $(this).find('.sub-menu').hide();
+            $(this).find('.sub-menu').slideUp();
             $(this).find('.arrow').removeClass('open');
         }else{
             $('.arrow').removeClass('open');
             $(this).addClass('open').siblings().removeClass('open');
-            $(this).find('.sub-menu').show();
+            $(this).find('.sub-menu').slideDown();
             $(this).find('.arrow').addClass('open');
         }
     });
